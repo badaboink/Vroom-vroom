74f001664a1ed2ed35a2f93449720ed660a442eb
Eglė Janušaityė <eglejanusaityte@gmail.com>
2022-05-09 14:35:11 +0300

    main activity redesign; to do: battery charge

--- app/src/main/java/com/example/myapplication/Battery.java
+++ app/src/main/java/com/example/myapplication/Battery.java
@@ -2,8 +2,13 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.service.autofill.Transformation;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +18,15 @@ public class Battery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_charging);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        Button text = (Button) findViewById(R.id.button);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+
+        });
+
     }
+
 }
