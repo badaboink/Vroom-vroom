9c4b7c2a1b3d74d98bc260b05e70328cf8813708
Eglė Janušaityė <eglejanusaityte@gmail.com>
2022-05-09 15:10:50 +0300

    Battery charges. Need to connect with payment

--- app/src/main/java/com/example/myapplication/Battery.java
+++ app/src/main/java/com/example/myapplication/Battery.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -12,17 +15,42 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class Battery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_charging);
+
+        // set battery percent to random
+        Random random = new Random();
+        int battery = random.nextInt(100);
         ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setProgress(battery);
+
+        // if button is pressed => charge
         Button text = (Button) findViewById(R.id.button);
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ValueAnimator animator = ValueAnimator.ofInt(battery, progressBar.getMax());
+                animator.setDuration(3000);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation){
+                        progressBar.setProgress((Integer)animation.getAnimatedValue());
+                    }
+                });
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        // start your activity here
+                    }
+                });
+                animator.start();
             }
 
         });
