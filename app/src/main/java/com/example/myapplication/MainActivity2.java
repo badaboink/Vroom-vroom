58d8b28aaef000f09428a2ddd515456e1bfbf9c0
Kornelija <kornelija.magylaite@gmail.com>
2022-05-25 23:12:11 +0300

    final shit

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,12 +31,19 @@ public class MainActivity2 extends AppCompatActivity {
     private TextView dateTV, priceTV;
     private Button getPriceBtn, setPriceBtn;
     private CardView priceCV;
-    private String username, sellToGrid, buyFromGrid;
+    private String username, sellToGrid, buyFromGrid, charging;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton1 = findViewById(R.id.charge);
+        radioButton2 = findViewById(R.id.discharge);
+
 
         dateTV = findViewById(R.id.idTVDate);
         priceTV = findViewById(R.id.idTVPrice);
@@ -51,22 +60,35 @@ public class MainActivity2 extends AppCompatActivity {
             username = usernameIDEdt.getText().toString();
             sellToGrid = sellToGridIDEdt.getText().toString();
             buyFromGrid = buyFromGridIDEdt.getText().toString();
+            if((!radioButton1.isChecked()) && (!radioButton2.isChecked())){
+                Toast.makeText(MainActivity2.this, "Nepasirinkote ar norite įkrauti ar iškrauti", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                if(radioButton1.isChecked()){
+                    charging = "yes";
 
-            if(TextUtils.isEmpty(username)){
-                usernameIDEdt.setError("Please enter username");
-            } else if (TextUtils.isEmpty(username)){
-                sellToGridIDEdt.setError("Please enter sell price");
-            } else if (TextUtils.isEmpty(username)){
-                buyFromGridIDEdt.setError("Please enter buy price");
-            } else {
-                setPrice(username, sellToGrid, buyFromGrid);
+                }
+                else if(radioButton2.isChecked()){
+                    charging = "no";
+
+                }
+                if(TextUtils.isEmpty(username)){
+                    usernameIDEdt.setError("Please enter username");
+                } else if (TextUtils.isEmpty(username)){
+                    sellToGridIDEdt.setError("Please enter sell price");
+                } else if (TextUtils.isEmpty(username)){
+                    buyFromGridIDEdt.setError("Please enter buy price");
+                }
+                else {
+                    setPrice(username, sellToGrid, buyFromGrid, charging);
+                }
             }
         });
     }
 
 
 
-    private void setPrice(String username, String sellToGrid, String buyFromGrid) {
+    private void setPrice(String username, String sellToGrid, String buyFromGrid, String charging) {
 
         String url = "http://192.168.245.121/writeElectricityPrices.php";
         RequestQueue queue = Volley.newRequestQueue(MainActivity2.this);
@@ -94,6 +116,7 @@ public class MainActivity2 extends AppCompatActivity {
                 params.put("username", username);
                 params.put("sellToGrid", sellToGrid);
                 params.put("buyFromGrid", buyFromGrid);
+                params.put("charging", charging);
                 return params;
             }
         };
